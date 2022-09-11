c0c6d538befa55ffaea21835ddc445150ca009d8
Alex Perronnet <aperronnet.login@gmail.com>
2022-09-12 01:19:22 +0200

    Add divider on table

--- src/components/projects-list.jsx
+++ src/components/projects-list.jsx
@@ -7,23 +7,23 @@ export default function ProjectsList() {
       <thead className="text-xs text-slate-400 uppercase bg-slate-700 border-b border-slate-600">
         <tr>
           <th className="hidden lg:table-cell lg:py-3 lg:px-6">No.</th>
-          <th className="py-3 px-6">Title</th>
-          <th className="py-3 px-6">Links</th>
-          <th className="hidden sm:table-cell sm:py-3 sm:px-6">Status</th>
+          <th className="py-3 px-6 lg:border-l lg:border-slate-600">Title</th>
+          <th className="py-3 px-6 border-l border-slate-600">Links</th>
+          <th className="hidden sm:table-cell sm:py-3 sm:px-6 border-l border-slate-600">Status</th>
         </tr>
       </thead>
       <tbody>
         {projects.map((project, index) => (
           <tr key={index} className="bg-slate-800 border-b border-slate-700 last:border-b-0">
             <td className="hidden lg:table-cell lg:p-6">{project.number}</td>
-            <td className="p-6">
+            <td className="p-6 lg:border-l lg:border-slate-700">
               <span className="hidden md:table-cell md:font-bold">{project.title}</span>
               <span className="font-bold md:font-normal md:text-xs md:text-slate-400 md:block md:mt-2">
                 {project.name}
               </span>
             </td>
             {project.status === 'Validated' ? (
-              <td className="p-6 text-slate-100">
+              <td className="p-6 text-slate-100  border-l border-slate-700">
                 <a href={project.links.source} className="mr-4 link-highlight">
                   Source
                 </a>
@@ -32,10 +32,12 @@ export default function ProjectsList() {
                 </a>
               </td>
             ) : (
-              <td className="p-6 text-slate-500 cursor-not-allowed line-through">Not available</td>
+              <td className="p-6 text-slate-500 cursor-not-allowed line-through border-l border-slate-700">
+                Not available
+              </td>
             )}
             <td
-              className={`hidden sm:table-cell sm:p-6 sm:text-xs ${
+              className={`hidden sm:table-cell sm:p-6 sm:text-xs border-l border-slate-700 ${
                 project.status === 'Validated' && 'sm:text-emerald-400'
               } ${project.status === 'In Progress' && 'sm:text-amber-400'} ${
                 project.status === 'Not Started' && 'sm:text-rose-400'
