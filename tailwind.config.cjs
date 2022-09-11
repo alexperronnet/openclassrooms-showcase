daad3ee26981ac8bb9cae73e547b701761af28fe
Alex Perronnet <aperronnet.login@gmail.com>
2022-09-11 03:10:46 +0200

    Setup layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+    },
+    container: {
+      center: true,
+      padding: '1.5rem',
+    },
+    fontFamily: {
+      sans: ['Space Grotesk', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
