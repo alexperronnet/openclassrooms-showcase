8c181c4c7b2970effce28f034043afc895e14864
Alex Perronnet <aperronnet.login@gmail.com>
2022-11-16 02:12:25 +0100

    Fix import names

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,6 +3,6 @@ export { default as Loader } from '~/components/loader';
 export { default as Beams } from '~/components/beams';
 export { default as Hero } from '~/components/hero';
 export { default as ToggleView } from '~/components/toggle-view';
-export { default as ProjectsGrid } from '~/components/projects.grid';
+export { default as ProjectsGrid } from '~/components/projects-grid';
 export { default as ProjectsList } from '~/components/projects-list';
 export { default as Footer } from '~/components/footer';
